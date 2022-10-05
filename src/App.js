031851d1b7db5eb6320f5b95f7c270403800eb40
JEETHENDRA KUMAR <jeethendrakumar1999@gmail.com>
2022-10-05 12:52:27 +0000

    Adding the nav bar to the portal.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import {
     Route,
 } from "react-router-dom";
 import Home from './components/home'
+import MainNavBar from './components/mainNav'
 
 
 class App extends React.Component{
     render(){
-        return <BrowserRouter>
-        <Routes>
-                    <Route exact path='/' element={<Home/>} />
-        </Routes>
-        </BrowserRouter>
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path='/' element={<MainNavBar/>} />
+                </Routes>
+            </BrowserRouter>
+        )
     }
 }
 
--- src/components/mainNav.jsx
+++ src/components/mainNav.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
-import Card from '../common/card'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
-class MainNav extends React.Component{
-    render(){
-        return <Card>hello</Card>
+const menu = [
+    {
+        platform: {
+            link: '/',
+            label: 'Home'
+        }
+    },
+    {
+        pricing: {
+            link: '/products',
+            label: 'Products'
+        }
+    },
+    {
+        resourses: {
+            link: '/services',
+            label: 'Services'
+        }
+    },
+    {
+        acceleration: {
+            link: '/about',
+            label: 'About Us'
+        }
+    }
+];
+
+const getMenu = () => {
+    return menu.map(items => (
+        (Object.values(items).map(item => {
+            return <li key={item.label} className="nav-item d-none d-sm-inline-block">
+                <NavLink exact to={item.link} className="nav-link thicker">
+                    {item.label}
+                </NavLink>
+            </li>
+        }))
+    ))
+}
+class MainNavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand bg-white navbar-light border-bottom">
+                <div className="brand-link">
+                    <img
+                        src='static/img/Alpha-designstyle-smoothie-m.png'
+                        alt="Klenty Logo"
+                        height="50"
+                        weight='100px'
+                        
+                    />
+                </div>
+                <ul className="navbar-nav ml-auto">
+                    {getMenu()}
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <div className='nav-item'>
+                        <button type="button" className="btn btn-outline-dark">
+                            CONTACT US
+                        </button>
+                        {'  '}
+                        <button type="button" className="btn btn-primary">
+                            TRY FOR FREE
+                        </button>
+                    </div>
+                </ul>
+            </nav>
+        );
     }
 }
 
-export default MainNav 
+MainNavBar.propTypes = {};
+
+
+export default (MainNavBar);
