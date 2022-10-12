84a0fcdbbe76d9b89f828bfb01feee7189f7880b
JEETHENDRA KUMAR <jeethendrakumar1999@gmail.com>
2022-10-12 04:07:24 +0000

    Added services component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const fetchApi = () => {
   return async (dispatch) => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const response = await axios.get('https://jsonplaceholder.typicode.com/comments')
     const data = response.data
     dispatch({type : 'FETCH_API',payload : data})
 }
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import fetchApi from '../../actions/index'
 import {connect} from 'react-redux'
+import Column from '../../common/column'
+import Card, {CardItem} from '../../common/card'
 
 class Services extends React.Component{
     componentDidMount(){
         this.props.fetchApi()
     }
     render(){
-        return <div className='content-wrapper'>{this.props.posts.map(items => {
-            return <li key={items.id}>
-                {items.title}</li>
-        })}</div>
+        return <CardItem item='body'>
+            <div className='row'>
+            {this.props.posts.map(items => {
+                return <Column span={2} key={items.it}>
+                    <Card>
+                        <CardItem item={'body'}>
+                        {items.email}
+                        </CardItem>
+                    </Card>
+                    </Column>
+            })}
+            </div>
+        </CardItem>
     }
 }
 
